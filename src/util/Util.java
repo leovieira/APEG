41fa7288a81062a19fd926dc5530b0efae6a1cb2
Vladimir <vladimir.iorio@gmail.com>
2013-02-11 20:24:25 -0200

    starting changes in grammar to deal with adding new rules

--- src/util/Util.java
+++ src/util/Util.java
@@ -98,7 +98,7 @@ public class Util {
 
 	}
 
-	
+/*	
 	public static void addRules(Grammar grammar, String rule)
 	throws Exception {
 		
@@ -118,7 +118,7 @@ public class Util {
 				
 
 	}
-
+*/
 
 	
 	
