1e096f5ec987f9e90eb0e9073b8abc480a6c4974
leovieira <crazymind@gmail.com>
2014-02-16 11:15:13 -0300

    printing the AST

--- project/syntax/test/apeg/syntax/TestSyntax.java
+++ project/syntax/test/apeg/syntax/TestSyntax.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.antlr.runtime.ANTLRFileStream;
 import org.antlr.runtime.CommonTokenStream;
 import org.antlr.runtime.RecognitionException;
+import org.antlr.runtime.tree.Tree;
 
 public class TestSyntax {
 
@@ -15,7 +16,9 @@ public class TestSyntax {
 		APEGLexer lexer = new APEGLexer(input);
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
 		APEGParser parser = new APEGParser(tokens);
-		parser.grammarDef();
+		APEGParser.grammarDef_return r = parser.grammarDef();
+		Tree t = r.getTree();
+		System.out.println(t.toStringTree());
 
 		System.out.println("End");
 	}
